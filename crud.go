266b077825de776f7bc73f9707b5a4bf3663fa44
goliatone <140582+goliatone@users.noreply.github.com>
2024-12-06 22:32:09 -0800

    add: new interfaces for request response

---
+++ crud.go
@@ -0,0 +1,53 @@
+package crud
+
+import (
+	"context"
+)
+
+type Request interface {
+	UserContext() context.Context
+	Params(key string, defaultValue ...string) string
+	BodyParser(out interface{}) error
+	Query(key string, defaultValue ...string) string
+	QueryInt(key string, defaultValue ...int) int
+	Queries() map[string]string
+}
+
+type Response interface {
+	Status(status int) Response
+	JSON(data interface{}, ctype ...string) error
+	SendStatus(status int) error
+}
+
+type Context interface {
+	Request
+	Response
+}
+
+type ResourceHandler interface {
+	// Index fetches all records
+	Index(Context) error
+	// Show fetches a single record, usually by ID
+	Show(Context) error
+	Create(Context) error
+	Update(Context) error
+	Delete(Context) error
+}
+
+// ResourceController defines an interface for registering CRUD routes
+type ResourceController[T any] interface {
+	RegisterRoutes(r Router)
+}
+
+// Router is a simplified interface from the crud package perspective, referencing the generic router
+type Router interface {
+	Get(path string, handler func(Context) error) RouterRouteInfo
+	Post(path string, handler func(Context) error) RouterRouteInfo
+	Put(path string, handler func(Context) error) RouterRouteInfo
+	Delete(path string, handler func(Context) error) RouterRouteInfo
+}
+
+// RouterRouteInfo is a simplified interface for route info
+type RouterRouteInfo interface {
+	Name(string) RouterRouteInfo
+}
